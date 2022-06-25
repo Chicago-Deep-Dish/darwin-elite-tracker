6e9f6bb4a81db6f84715fe3e00071fd4f1d8f8be
gilcohen67 <gil.cohen67@gmail.com>
2022-06-25 16:15:50 -0700

    added working darkmode with a few choices

--- src/App.js
+++ src/App.js
@@ -4,17 +4,23 @@ import HomePage from './pages/Homepage';
 import RecordLibrary from './pages/RecordLibrary'
 import Theme from './Context/ThemeContext';
 import Layout from './Layout';
+import { GlobalContextProvider } from './Context/GlobalContext';
+import { Box } from '@mui/material';
 
 export default function App() {
   return (
     <div className="App">
       <Theme>
-        <Layout>
-          <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/records' element={<RecordLibrary />} />
-          </Routes>
-        </Layout>
+        <GlobalContextProvider>
+          <Box sx={{ backgroundColor: 'secondary.blueish', height: '100vh', width: '100vw' }}>
+            <Layout>
+              <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/records' element={<RecordLibrary />} />
+              </Routes>
+            </Layout>
+          </Box>
+        </GlobalContextProvider>
       </Theme>
     </div>
   );
