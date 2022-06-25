f2b12da831ecf6adc796bd909e6334b45ab8c873
Jerry Tapia <jerryxtapia@gmail.com>
2022-06-24 19:49:33 -0700

    created front end routes

---
+++ src/components/Layout.js
@@ -0,0 +1,11 @@
+function Layout (props) {
+  return (
+    <div>
+      <div>
+        {props.children}
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
---
+++ src/pages/RecordLibrary.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+
+function RecordLibrary() {
+  return (
+    <Layout>
+      <h1>Records</h1>
+    </Layout>
+  );
+}
+
+export default RecordLibrary;
