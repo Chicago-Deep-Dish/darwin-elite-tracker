435b157a5c4a1cc7ee3b980fe0053cc3f9847e45
gilcohen67 <gil.cohen67@gmail.com>
2022-06-25 16:19:40 -0700

    moved dark background to Layout component

--- src/App.js
+++ src/App.js
@@ -5,21 +5,18 @@ import RecordLibrary from './pages/RecordLibrary'
 import Theme from './Context/ThemeContext';
 import Layout from './Layout';
 import { GlobalContextProvider } from './Context/GlobalContext';
-import { Box } from '@mui/material';
 
 export default function App() {
   return (
     <div className="App">
       <Theme>
         <GlobalContextProvider>
-          <Box sx={{ backgroundColor: 'secondary.blueish', height: '100vh', width: '100vw' }}>
             <Layout>
               <Routes>
                 <Route path='/' element={<HomePage />} />
                 <Route path='/records' element={<RecordLibrary />} />
               </Routes>
             </Layout>
-          </Box>
         </GlobalContextProvider>
       </Theme>
     </div>
