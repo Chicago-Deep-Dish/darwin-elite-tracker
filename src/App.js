e2e40fc00ef42ce20352547ae042909953e1a66a
Xiaohuan <huxiaohuan.jlu@gmail.com>
2022-06-27 08:35:39 -0700

    updated theme

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import RecordLibrary from './pages/RecordLibrary'
 import Theme from './Context/ThemeContext';
 import Layout from './Layout';
 import MenueBar from './components/HomePage/HomeGraphs/MenueBar.js';
+import { GlobalContextProvider } from './Context/GlobalContext';
 
 export default function App() {
   return (
     <div className="App">
       <Theme>
+       <GlobalContextProvider>
         <Layout>
           <Routes>
             <Route path='/' element={<HomePage />} />
@@ -17,6 +19,7 @@ export default function App() {
           </Routes>
           <MenueBar/ >
         </Layout>
+       </GlobalContextProvider>
       </Theme>
 
     </div>
