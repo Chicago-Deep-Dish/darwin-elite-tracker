d95cd4aa037904cb3358cd96cda27dc7a988181b
Junsu Park <junsupark94@gmail.com>
2022-06-30 17:40:06 -0700

    Fixed Bar Graph

--- src/components/HomePage/HomeGraphs/BarGraph.js
+++ src/components/HomePage/HomeGraphs/BarGraph.js
@@ -9,7 +9,6 @@ import useGlobalContext from '../../../context/GlobalContext.js'
 
 export default function Bar() {
   const { userProblemArray } = useGlobalContext();
- console.log( 'dattaaaaaa',userProblemArray);
   const [graph, setGraph] = React.useState('totalTime');
   const [selection, setSelection]=React.useState('');
   const [subject, setSubject] = React.useState([]);
@@ -18,185 +17,101 @@ export default function Bar() {
   const [range, setRange]=React.useState('week');
   const [language, setLanguage]=React.useState('Javascript');
 
-  const handleTime = (event) => {
-    console.log('hit1', event.target.value);
-    setTime(event.target.value);
-  };
-  const handleRange= (event) => {
-    console.log('hit2',event.target.value);
-    setRange(event.target.value);
-  };
-  const handleLanguage = (event) => {
-    console.log('hit3',event.target.value);
-    setLanguage(event.target.value);
-  };
-
-  const handleGraph = (event) => {
-    console.log('hit4',event.target.value);
-    setGraph(event.target.value);
-  };
-  const handleSelection= (event) => {
-    console.log('hit5',event.target.value);
-    setSelection(event.target.value);
-    setSubject([]);
-  };
-  const handleSubject= (event) => {
-    console.log('hit6',event.target.value);
-    setSubject(event.target.value);
-  };
-
- const getLastDate = (x)=> {
+  const getLastDate = (x)=> {
     const now = new Date();
     const result=new Date(now.getFullYear(), now.getMonth(), now.getDate() - x);
     return result.toISOString();
+  }
+
+  React.useEffect(() => {
+    var easy = 0;
+    var medium = 0;
+    var hard = 0;
+    var lastDate=getLastDate(0);
+    var startDate=getLastDate(6);
+    if (range === 'month') {
+      startDate = getLastDate(29);
+    } else if (range === 'year') {
+      startDate = getLastDate(364);
     }
+    const timeFiltered = userProblemArray.filter(problem => (problem.timeStamp > startDate && problem.timeStamp < lastDate));
+    const timeAndLangFiltered = timeFiltered.filter(sample => sample["programmingLanguage"] === language);
 
-  var easy = 0;
-  var medium = 0;
-  var hard = 0;
-  var lastDate=getLastDate(0);
-  var startDate=getLastDate(6);
-
-  React.useEffect ( ()=>{
-    var samples=[];
-    console.log('easy,hard,medium', easy, medium, hard);
-    //send request during 'range' time with 'language' for  as data
-   // console.log('subject',subject);
-
-   //filter range
-    if ( range==='week') {
-      for (let i=0; i< userProblemArray.length; i++) {
-        if( userProblemArray[i]['timeStamp']>startDate&&userProblemArray[i]["timeStamp"]<lastDate) {
-          samples.push(userProblemArray[i]);
+    //filter total&subject
+    if (graph === 'totalQuantities' && selection === 'subject') {
+      var result = Array(subject.length).fill(0);
+      timeAndLangFiltered.forEach(problem => {
+        var index = subject.indexOf(problem.topics);
+        if (index !== -1) {
+          result[index]++;
         }
-      }
-    }else if ( range==='month') {
-      startDate=getLastDate(29);
-      for (let i=0; i< userProblemArray.length; i++) {
-        if( userProblemArray[i]['timeStamp']>startDate&&userProblemArray[i]["timeStamp"]<lastDate) {
-          samples.push(userProblemArray[i]);
+      })
+      setInput(result);
+    } //working
+
+    if (graph === 'totalQuantities' && selection === 'difficulty') {
+      timeAndLangFiltered.forEach(problem => {
+        if(problem.difficulty === 'easy') easy++;
+        else if (problem.difficulty === 'medium') medium++;
+        else hard++;
+      })
+      setInput([easy, medium, hard]);
+    }//working
+
+    if (graph === 'totalTime' && selection === 'subject') {
+      var totalTime = Array(subject.length).fill(0);
+      var count=Array(subject.length).fill(0);
+      timeAndLangFiltered.forEach(problem => {
+        const index = subject.indexOf(problem.topics);
+        if (index !== -1) {
+          totalTime[index] = parseInt(problem.totalTime);
+          count[index]++;
         }
-      }
-    }else if ( range === 'year') {
-      startDate=getLastDate(364);
-      for (let i=0; i< userProblemArray.length; i++) {
-        if( userProblemArray[i]['timeStamp']>startDate&&userProblemArray[i]["timeStamp"]<lastDate) {
-          samples.push(userProblemArray[i]);
+      });
+      totalTime = totalTime.map((time, i) => {
+        if(time !== 0) {
+          time /= count[i]; // Junsu: average
+          time /= 1000; // Junsu: 1000 milliseconds
+          time /= 60; // Junsu: 60 seconds
         }
+        return time;
+      })
+      setInput(totalTime);
+    }
+
+    if(graph === 'totalTime' && selection === 'difficulty') {
+      var countE = 0, countM = 0, countH = 0;
+      timeAndLangFiltered.forEach(problem => {
+        if (problem.difficiulty === 'easy') {
+          easy += parseInt(problem.totalTime);
+          countE++;
+        } else if (problem.difficulty === 'medium') {
+          medium += parseInt(problem.totalTime);
+          countM++;
+        } else {
+          hard += parseInt(problem.totalTime);
+          countH++;
+        }
+      });
+    //get average and change to minus
+      if(countE!==0){
+        easy /= countE;
+        easy /= 1000;
+        easy /= 60;
       }
-    }
-  //console.log( 'rangggge' , range, samples);
-  console.log('SAAAAAAMples', samples)
-  //filter the language
-  var sampleUpdate=[];
-  for ( let i=0; i<samples.length; i++) {
-    if ( samples[i]['programmingLanguage']!==undefined) {
-      if(samples[i]['programmingLanguage'].toLowerCase()===language.toLowerCase()) {
-      sampleUpdate.push(samples[i]);
-    }
-  }
-}
-  console.log('updated', sampleUpdate)
-
-  //filter total&subject
-if (graph==='totalQuantities'&&selection==='subject') {
-  for (let i=0; i<sampleUpdate.length; i++) {
-    var result=Array(subject.length).fill(0);
-    var sub=sampleUpdate[i]['topics'];
-    console.log('subbb', sub, result);
-    var index=subject.indexOf(sub);
-    if(index>=0) {
-    result[index]++;
-    }
-  }
-    console.log( 'filter subject and total', result);
-    setInput(result);
-} //working
-
-if (graph==='totalQuantities'&&selection==='difficulty') {
-  //axios.get('/total', { params:{"range":range,'language':language}})
-
-  for ( let i=0; i<sampleUpdate.length; i++) {
-    if (sampleUpdate[i].difficulty.toLowerCase()==='easy') {
-      easy++;
-    } else if ( sampleUpdate[i].difficulty.toLowerCase()==='medium') {
-      medium++;
-    }else {
-      hard++;
-    }
-  }
-  console.log('difficulty & total', [easy, medium, hard])
-  setInput([easy, medium, hard]);
-
-}//working
-
-if (graph==='totalTime'&&selection==='subject') {
- // axios.get('/total', { params:{'selection':subject, "range":range,'language':language}})
- var totalTime;
- for (let i=0; i<sampleUpdate.length; i++) {
-  totalTime=Array(subject.length).fill(0);
-  var count=Array(subject.length).fill(0);
-  var sub=sampleUpdate[i]['topics'];
-  //console.log('subbb', sub, totalTime);
-  var index=subject.indexOf(sub);
-  if(index>=0) {
-  totalTime[index]=totalTime[index]+sampleUpdate[i]['totalTime'];
-  count[index]++;
-  }
-}
-console.log('hit7', sampleUpdate);
-console.log('hit6', totalTime)
-if (totalTime) {
-for (let i=0; i<totalTime.length; i++) {
-  if (totalTime[i]!==0) {
-    totalTime[i]=totalTime[i]/count[index];
-    totalTime[i]=totalTime[i]/1000/60;
-  }
-}
-}
-  console.log( 'filter subject and total', totalTime);
-  setInput(totalTime);
-
-}
-
-if(graph==='totalTime'&&selection==='difficulty') {
-  var countE=0;
-  var countM=0;
-  var countH=0;
-  //axios.get('/total', { params:{"range":range,'language':language}})
-  for ( let i=0; i<sampleUpdate.length; i++) {
-    if (sampleUpdate[i].difficulty.toLowerCase()==='easy') {
-      easy = easy + sampleUpdate[i]["totalTime"];
-      countE++;
-    } else if ( sampleUpdate[i].difficulty.toLowerCase()==='medium') {
-      medium = medium + sampleUpdate[i]["totalTime"];
-      countM++;
-    }else {
-      hard = hard + sampleUpdate[i]["totalTime"];
-      countH++
-    }
-  }
-//get average and change to minus
-  if(countE!==0){
-    easy=easy/countE;
-    easy=easy/1000/60;
-  }
-  if(countM!==0){
-    medium=medium/countM;
-    medium=medium/1000/60;
-  }
-  if(countH!==0){
-    hard=hard/countH;
-    hard=hard/1000/60;
-  }
-
-  setInput([easy, medium, hard]);
-
-}//done but not checked
-
-// console.log('state', [easy, medium, hard]);
-// console.log('testtt', state.speed)
-}, [graph,selection, subject, time, language,range])
+      if(countM!==0){
+        medium /= countM;
+        medium /= 1000;
+        medium /= 60;
+      }
+      if(countH!==0){
+        hard /= countH;
+        hard /= 1000;
+        hard /= 60;
+      }
+      setInput([easy, medium, hard]);
+    }//Junsu: working now
+  }, [graph, selection, subject, time, language, range])
 
   const option = {
     title:{
@@ -224,13 +139,30 @@ if(graph==='totalTime'&&selection==='difficulty') {
       }
     ]
   }
-return (
-  <Stack >
-    <Box sx={{ '&:hover':{boxShadow:5},   width:'500px', ml:4, mr:4, mt:1,mb:2, backgroundColor: 'black'}}>
-      <ReactEcharts option={option} />
-    </Box>
-    <MenuBar  graph={graph} setGraph={setGraph} subject= {subject} handleSubject={handleSubject} selection={selection} setSelection={setSelection} time={time} range={range} language={language} handleRange={handleRange} handleLanguage={handleLanguage} handleTime={handleTime} handleGraph={handleGraph} handleSelection={handleSelection}/>
-
-  </Stack>
-)
+  return (
+    <Stack >
+      <Box sx={{ '&:hover':{boxShadow:5},   width:'500px', ml:4, mr:4, mt:1,mb:2, backgroundColor: 'black'}}>
+        <ReactEcharts option={option} />
+      </Box>
+      <MenuBar
+        graph={graph}
+        subject= {subject}
+        selection={selection}
+        time={time}
+        range={range}
+        language={language}
+        setGraph={setGraph}
+        setSelection={setSelection}
+        handleSubject={(e) => setSubject(e.target.value)}
+        handleRange={(e) => setRange(e.target.value)}
+        handleLanguage={(e) => setLanguage(e.target.value)}
+        handleTime={(e) => setTime(e.target.value)}
+        handleGraph={(e) => setGraph(e.target.value)}
+        handleSelection={(e) => {
+          setSelection(e.target.value);
+          setSubject([]);
+        }}
+      />
+    </Stack>
+  )
 }
